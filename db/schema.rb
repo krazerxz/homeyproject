173e9cbdce4414f1aec4f060f02f7df920d655b7
Chris Pomfret <cpom40ba@gmail.com>
2025-02-17 20:55:58 +0000

    Install paper_trail

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_15_120740) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_15_135853) do
   create_table "comments", force: :cascade do |t|
     t.text "body"
     t.integer "project_id", null: false
@@ -23,6 +23,16 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_15_120740) do
     t.integer "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "versions", force: :cascade do |t|
+    t.string "whodunnit"
+    t.datetime "created_at"
+    t.bigint "item_id", null: false
+    t.string "item_type", null: false
+    t.string "event", null: false
+    t.text "object", limit: 1073741823
+    t.index ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id"
   end
 
   add_foreign_key "comments", "projects"
